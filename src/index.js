394b9ed3bc944451bc8d53fe4392278c79672e77
Yuanda <yuandaxu@yahoo.com>
2023-04-19 20:40:53 -0400

    implement

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+class HelloMessage extends React.Component {
+    render() {
+        return React.createElement("div", null, "Hello World");
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"))
+root.render(React.createElement(HelloMessage, null))
